d4a6df4e4673d5137cfaddb6a029a45fd425b674
Vishant <vishantmehta0@gmail.com>
2025-09-02 19:50:28 +0530

    Tested and Implemented Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+connectDB();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
-    res.json({ message: "API is running" });
+    res.send('API is running...');
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
